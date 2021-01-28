567d100f74ff8d52847f01802838db049e48396f
sanadwj <s_wj_88@hotmail.com>
2021-01-28 21:36:44 +0200

    Create Components folder
    Add Display component content

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Display from './Display';
+
+function App() {
+  return (
+    <>
+      <Display />
+      <h1>Hello World</h1>
+    </>
+  );
+}
+
+export default App;

d7aa1b75c751139d9bdbad9a3072f670276863b1
sanadwj <s_wj_88@hotmail.com>
2021-02-02 19:21:48 +0200

    Convert App function to a class component

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,25 @@ import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 // import calculate from '../logic/calculate';
 
-function App() {
-  return (
-    <>
-      <Display />
-      <ButtonPanel />
-    </>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  render() {
+    return (
+      <>
+        <Display />
+
+        <ButtonPanel />
+      </>
+    );
+  }
 }
 
 export default App;

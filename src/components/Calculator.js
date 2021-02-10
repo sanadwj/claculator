4d33af6d4b8871841220acc861eea70cf1172576
sanadwj <s_wj_88@hotmail.com>
2021-02-10 15:32:48 +0200

    Add react-router package
    Move app content to calculator

---
+++ src/components/Calculator.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import Display from './Display';
+import ButtonPanel from './ButtonPanel';
+import calculate from '../logic/calculate';
+import './App.css';
+
+const Calculator = () => {
+  const [state, setState] = useState({
+    total: '',
+    operation: '',
+    next: '',
+  });
+
+  const handleClick = buttonName => {
+    const result = calculate(state, buttonName);
+    setState(result);
+  };
+
+  return (
+    <>
+      <div className="calculator">
+        <Display total={state.total} operation={state.operation} next={state.next} />
+        <ButtonPanel clickHandler={handleClick} />
+      </div>
+    </>
+
+  );
+};
+
+export default Calculator;
